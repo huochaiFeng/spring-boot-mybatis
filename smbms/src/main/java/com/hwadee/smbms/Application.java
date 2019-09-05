fbd84f68fb4695440bf712fff023d799d984621c
fengzhengzhong <fengzhengzhongscgz@163.com>
2019-09-05 11:05:36 +0800

    添加userController控制器

--- smbms/src/main/java/com/hwadee/smbms/Application.java
+++ smbms/src/main/java/com/hwadee/smbms/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.hwadee.smbms.dao")
+@MapperScan("com.hwadee.smbms.mapper")
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
